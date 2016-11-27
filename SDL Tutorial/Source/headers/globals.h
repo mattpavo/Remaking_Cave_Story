8692e93110b75a6400bec4f2433d4db626a710ae
Matthew Pavouris <mattpavo@yahoo.ca>
2016-11-27 14:44:55 -0500

    Created 'Vector2' structure

--- SDL Tutorial/Source/headers/globals.h
+++ SDL Tutorial/Source/headers/globals.h
@@ -10,7 +10,16 @@ namespace globals {
 }
 
 struct Vector2 {
-
+	int x, y;
+	Vector2() :
+		x(0), y(0)
+	{}
+	Vector2(int x, int y) :
+		x(x), y(y)
+	{}
+	Vector2 zer() {
+		return Vector2(0, 0);
+	}
 };
 
 #endif
