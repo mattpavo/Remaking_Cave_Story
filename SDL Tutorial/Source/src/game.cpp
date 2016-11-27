c696d111b3e36b9696293f10454027921827182b
Matthew Pavouris <mattpavo@yahoo.ca>
2016-11-27 18:22:37 -0500

    Added "animatedsprite.h" and "animatedsprite.cpp"; made character run in place

--- SDL Tutorial/Source/src/game.cpp
+++ SDL Tutorial/Source/src/game.cpp
@@ -7,7 +7,6 @@ This class holds all information for our main game loop
 #include "graphics.h"
 #include "input.h"
 #include <algorithm>
-#include <SDL_image.h>
 #include <string>
 
 namespace
@@ -33,7 +32,9 @@ void Game::gameLoop()
 	Input input;
 	SDL_Event event;
 
-	this->_player = Sprite(graphics, "MyChar.png", 0, 0, 16, 16, 100, 100);
+	this->_player = AnimatedSprite(graphics, "MyChar.png", 0, 0, 16, 16, 100, 100, 100);
+	this->_player.setupAnimations();
+	this->_player.playAnimation("RunRight");
 
 	int LAST_UPDATE_TIME = SDL_GetTicks();
 	
@@ -84,5 +85,6 @@ void Game::draw(Graphics &graphics) {
 
 void Game::update(float elapsedTime)
 {
+	this->_player.update(elapsedTime);
 
 }
