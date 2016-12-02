9b5961ec62bf105b7c8db198515bb8244600d523
Matthew Pavouris <mattpavo@yahoo.ca>
2016-12-01 22:36:01 -0500

    Added 'bkBlue' background; added level to game class; fixed Quote's transparency

--- SDL Tutorial/Source/src/level.cpp
+++ SDL Tutorial/Source/src/level.cpp
@@ -14,15 +14,13 @@ Level::Level(std::string mapName, Vector2 spawnPoint, Graphics &graphics) :
 	this->loadMap(mapName, graphics);
 }
 
-Level::Level() {
-
-}
+Level::~Level() {}
 
 void Level::loadMap(std::string mapName, Graphics &graphics) {
 	//TEMPORARY CODE TO LOAD THE BACKGROUND
 	this->_backgroundTetxture = 
 		SDL_CreateTextureFromSurface(graphics.getRenderer(), 
-		graphics.loadImage("content/background/bkBlue.png"));
+		graphics.loadImage("bkBlue.png"));
 	this->_size = Vector2(1280, 960);
 }
 
@@ -32,5 +30,16 @@ void Level::update(int elapsedTime) {
 
 void Level::draw(Graphics &graphics) {
 	//Draw the background
-	SDL_Rect sourceRect = {};
+	SDL_Rect sourceRect = {0, 0, 64, 64};
+	SDL_Rect destRect;
+	for (int x = 0; x < this->_size.x / 64; x++) {
+		for (int y = 0; y < this->_size.y / 64; y++) {
+			destRect.x = x * 64;
+			destRect.y = y * 64;
+			destRect.w = 64;
+			destRect.h = 64;
+			graphics.blitSurface(this->_backgroundTetxture,
+			&sourceRect, &destRect);
+		}
+	}
 }
